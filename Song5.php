3abcc4c71973069d261e9b4a6e74e696917c0825
Karylle Manrique <manriquekarylle891@gmail.com>
2022-05-13 21:50:36 +0800

    Song5

--- Song5.php
+++ Song5.php
@@ -12,24 +12,24 @@
     </head>
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+        <nav class="navbar navbar-expand-lg navbar-white bg-white fixed-top" id="mainNav">
             <div class="container px-4">
                 <a class="navbar-brand" href="#page-top"> Melody Log </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-light_gray" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light_gray" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light_gray" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light_gray" href="song3.php">Song 3</a></li>
+                        <li class="nav-item"><a class="nav-link text-light_gray" href="song4.php">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light_gray" href="song5.php">Song 5</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
         <!-- Header-->
-        <header class="bg-dark bg-gradient text-white">
+        <header class="bg-secondary bg-gradient text-white">
             <div class="container px-4 text-center">
                 <h1 class="fw-bolder">Welcome to Melody Log</h1>
                 <p class="lead">“Music is the life itself.” </p>
@@ -141,7 +141,7 @@
             </div>
         </section>
 
-        <footer class="py-4 bg-danger">
+        <footer class="py-4 bg-secondary">
             <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Karylle M. Manrique </p></div>
         </footer>
         <!-- Bootstrap core JS-->
