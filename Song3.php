406497d7dc4d5335fe9b623cf555447509204045
Karylle Manrique <manriquekarylle891@gmail.com>
2022-05-13 18:34:49 +0800

    Song3

---
+++ Song3.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Melody Log </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melody Log</h1>
+                <p class="lead">“Music is the life itself.” </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Comethru</h2>";
+                        echo "<p class='lead '>Jeremy Zucker</p>";
+                        echo "<br/>";
+                        
+                        //Verse
+                        echo "<p> Verse</p>";
+                        echo "<p> I might lose my mind </p>";
+                        echo "<p> Waking when the sun's down</p>";
+                        echo "<p> Riding all these highs</p>";
+                        echo "<p> Waiting for the comedown</p>";
+                        echo "<p> Walk these streets with me</p>";
+                        echo "<p> I'm doing decently</p>";
+                        echo "<p> Just glad that I can breathe, yeah</p>";
+                        echo "<br/>";
+
+                        //Pre Chorus
+                        echo "<p> Pre-Chorus</p>";
+                        echo "<p> I'm trying to realize</p>";
+                        echo "<p> It's alright to not be fine on your own</p>";
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus</p>";
+                        echo "<p> Now I'm shaking, drinking all this coffee</p>";
+                        echo "<p> These last few weeks have been exhausting</p>";
+                        echo "<p> I'm lost in my imagination</p>";
+                        echo "<p> And there's one thing that I need from you</p>";
+                        echo "<p> Can you come through, through?</p>";
+                        echo "<p> Through, yeah</p>";
+                        echo "<p> And there's one thing that I need from you</p>";
+                        echo "<p> Can you come through?</p>";
+                        echo "<br/>";
+
+                        //Verse
+                        echo "<p> Verse</p>";
+                        echo "<p> Ain't got much to do</p>";
+                        echo "<p> Too old for my hometown</p>";
+                        echo "<p> Went to bed at noon</p>";
+                        echo "<p> Couldn't put my phone down</p>";
+                        echo "<p> Scrolling patiently</p>";
+                        echo "<p> It's all the same to me</p>";
+                        echo "<p> Just faces on a screen, yeah</p>";
+                        echo "<br/>";
+
+                        //Pre Chorus
+                        echo "<p> Pre-Chorus</p>";
+                        echo "<p> I'm trying to realize</p>";
+                        echo "<p> It's alright to not be fine on your own</p>";
+
+                         //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus</p>";
+                        echo "<p> Now I'm shaking, drinking all this coffee</p>";
+                        echo "<p> These last few weeks have been exhausting</p>";
+                        echo "<p> I'm lost in my imagination</p>";
+                        echo "<p> And there's one thing that I need from you</p>";
+                        echo "<p> Can you come through, through?</p>";
+                        echo "<p> Through, yeah</p>";
+                        echo "<p> And there's one thing that I need from you</p>";
+                        echo "<br/>";
+
+                        echo "<p> Can you come</p>";
+                        echo "<p> Through, through</p>";
+                        echo "<p> Through, yeah</p>";
+                        echo "<p> And there's one thing that I need from you</p>";
+                        echo "<p> Can you come through?</p>";
+                        echo "<br/>";
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Karylle M. Manrique </p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
