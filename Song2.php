68a9a86fc573242d8bdbb7d20b53c94779334e74
Karylle Manrique <manriquekarylle891@gmail.com>
2022-05-13 17:04:11 +0800

    Song2

---
+++ Song2.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Melody Log </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Melody Log</h1>
+                <p class="lead">“Music is the life itself.” </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Still</h2>";
+                        echo "<p class='lead '>Jeff Bernat</p>";
+                        echo "<br/>";
+                        
+                        //Verse
+                        echo "<p> Verse</p>";
+                        echo "<p> It's been a long time since we last spoke </p>";
+                        echo "<p> I still wonder what you're up to</p>";
+                        echo "<p> After all the shit we've been through</p>";
+                        echo "<p> And I've been spending a lot of time drinking and smoking</p>";
+                        echo "<p> Hoping to subdue</p>";
+                        echo "<p> The thought of you in my head</p>";
+                        echo "<br/>";
+
+                        //Pre Chorus
+                        echo "<p> Pre-Chorus</p>";
+                        echo "<p> So if you're out there somewhere</p>";
+                        echo "<p> And if you still even care</p>";
+                        echo "<p> Possibly still questioning what is he doing</p>";
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus</p>";
+                        echo "<p> I've been tryna find a way to numb all this pain</p>";
+                        echo "<p> Since you've been away</p>";
+                        echo "<p> I don't wanna feel anymore</p>";
+                        echo "<p> And there's probably people telling you that I'm doing fine</p>";
+                        echo "<p> Just taking my time</p>";
+                        echo "<p> Although it appears that I'm strong</p>";
+                        echo "<p> It still hurts</p>";
+                        echo "<br/>";
+
+                        //Verse
+                        echo "<p> Verse</p>";
+                        echo "<p> There's been a lot of nights when I feel alone</p>";
+                        echo "<p> Needed someone to talk to</p>";
+                        echo "<p> 'Cause I just need to get through</p>";
+                        echo "<p> What'cha say, starry-eyed</p>";
+                        echo "<p> And I've been spending a lot of money</p>";
+                        echo "<p> On things that I don't need</p>";
+                        echo "<p> Hoping that I'll feel new</p>";
+                        echo "<p> But I can't get you out my head</p>";
+                        echo "<br/>";
+
+                        //Pre Chorus
+                        echo "<p> Pre-Chorus</p>";
+                        echo "<p> So if you're out there somewhere</p>";
+                        echo "<p> And if you still even care</p>";
+                        echo "<p> Possibly still questioning what is he doing</p>";
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus</p>";
+                        echo "<p> I've been tryna find a way to numb all this pain</p>";
+                        echo "<p> Since you've been away</p>";
+                        echo "<p> I don't wanna feel anymore</p>";
+                        echo "<p> And there's probably people telling you that I'm doing fine</p>";
+                        echo "<p> Just taking my time</p>";
+                        echo "<p> Although it appears that I'm strong</p>";
+                        echo "<br/>";
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus</p>";
+                        echo "<p> I've been tryna find a way to numb all this pain</p>";
+                        echo "<p> Since you've been away (a way that I can numb this pain)</p>";
+                        echo "<p> I don't wanna feel anymore (I don't wanna feel)</p>";
+                        echo "<p> And there's probably people telling you that I'm doing fine  (I'm just tryna get you outta my head)</p>";
+                        echo "<p> Just taking my time</p>";
+                        echo "<p> Although it appears that I'm strong</p>";
+                        echo "<p> It still hurts</p>";
+                        echo "<br/>";
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Karylle M. Manrique </p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
