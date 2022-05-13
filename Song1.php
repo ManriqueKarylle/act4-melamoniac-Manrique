52ba57460e0b5760411ca3d7e2a22052d425f3c2
Karylle Manrique <manriquekarylle891@gmail.com>
2022-05-13 16:18:21 +0800

    index

---
+++ Song1.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Tune Station </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Tune Station</h1>
+                <p class="lead">“Music is the soundtrack of your life.” </p>
+                
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Summer Song</h2>";
+                        echo "<p class='lead '>Silent Sanctuary</p>";
+                        echo "<br/>";
+                
+                        echo "<p> Magkatabi tayo sa duyan </p>";
+                        echo "<p> Sa ilalim ng buwan</p>";
+                        echo "<p> Buhangin sa ating mga paa</p>";
+                        echo "<p> Ang dagat ay kumakanta</p>";
+                        echo "<p> Matagal naring magkakilala</p>";
+                        echo "<p> Minahal na kita</p>";
+                        echo "<p> Simula pa nung unang..</p>";
+                        echo "<p> Unang makita ang iyong mga mata</p>";
+                        echo "<br/>";
+
+
+                        //Pre Chorus
+                        echo "<p> Pre-Chorus</p>";
+                        echo "<p> Sana ay h'wag nang matapos tong</p>";
+                        echo "<p> Pagibig na para lamang sa iyo</p>";
+                        
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus</p>";
+                        echo "<p> Gusto kong tumalon, tumalon sa saya dahil</p>";
+                        echo "<p> Ikaw ang kapiling</p>";
+                        echo "<p> Sa iyo, sa iyo, sa iyo lamang</p>";
+                        echo "<p> Ang puso ko</p>";
+                        echo "<p> Sumayaw, sumayaw, sumayaw tayo</p>";
+                        echo "<p> Sa ilalim ng araw</p>";
+                        echo "<br/>";
+
+
+                        echo "<p> Tuwing ika'y nalulungkot</p>";
+                        echo "<p> Nandito lang ako pangako ko sa'yo</p>";
+                        echo "<p> Hindi kita iiwan</p>";
+                        echo "<p> Huwag kang mag-alala (huwag kang mag-alala)</p>";
+                        echo "<p> Gusto mo ng beer ililibre kita (sige na, sige na, sige na)</p>";
+                        echo "<p> Basta't ika'y kasama, di ako mangangamba</p>";
+                        echo "<p> Kislap ng yong mata tibok ng puso'y sumaya</p>";
+                        echo "<br/>";
+
+                        echo "<p> Ikaw lang ang aking mamahalin</p>";
+                        echo "<p> Hanggang sa langit ikaw ay dadalhin</p>";
+                        echo "<p> Tara na, tara na, tara na</p>";
+                        echo "<br/>";
+
+                        //Pre Chorus
+                        echo "<p> Pre-Chorus</p>";
+                        echo "<p> Sana ay h'wag nang matapos tong</p>";
+                        echo "<p> Pagibig na para lamang sa iyo</p>";
+
+                        //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus</p>";
+                        echo "<p> Gusto kong tumalon, tumalon sa saya dahil</p>";
+                        echo "<p> Ikaw ang kapiling</p>";
+                        echo "<p> Sa iyo, sa iyo, sa iyo lamang</p>";
+                        echo "<p> Ang puso ko</p>";
+                        echo "<p> Sumayaw, sumayaw, sumayaw tayo</p>";
+                        echo "<p> Sa ilalim ng araw</p>";
+                        echo "<br/>";
+
+                        echo "<p> Magkatabi tayo sa duyan</p>";
+                        echo "<p> Sa ilalim ng buwan</p>";
+                        echo "<p> Buhangin sa ating mga paa</p>";
+                        echo "<p> Ang dagat ay kumakanta</p>";
+                        echo "<br/>";
+
+                        echo "<p> Matagal naring magkakilala</p>";
+                        echo "<p> Minahal na kita</p>";
+                        echo "<p> Simula pa nung una</p>";
+                        echo "<p> Unang makita ang iyong mga mata</p>";
+                        echo "<br/>";
+
+                        //Pre Chorus
+                        echo "<p> Pre-Chorus</p>";
+                        echo "<p> Sana ay h'wag nang matapos tong</p>";
+                        echo "<p> Pagibig na para lamang sa iyo</p>";
+
+                         //Chorus
+                        echo "<br/>";
+                        echo "<p> Chorus</p>";
+                        echo "<p> Gusto kong tumalon, tumalon sa saya dahil</p>";
+                        echo "<p> Ikaw ang kapiling</p>";
+                        echo "<p> Sa iyo, sa iyo, sa iyo lamang</p>";
+                        echo "<p> Ang puso ko</p>";
+                        echo "<p> Sumayaw, sumayaw, sumayaw tayo</p>";
+                        echo "<p> Sa ilalim ng araw</p>";
+                        echo "<p> Sa ilalim ng araaaaaaawwwwww</p>";
+                        echo "<br/>";
+
+                        ?>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-4 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Patrick Lance Manuel G. Sadiwa </p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<?php
+SESSION_START();
+//Song Titles
+$song_lyrics1 = "Easy";
+$song_lyrics2 = "Still";
+$song_lyrics3 = "Comethru";
+$song_lyrics4 = "Casual";
+$song_lyrics5 = "Ligaya";
+//current url redirection
+$url_add = "http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+
+
+//condition to know if the button is clicked
+if(isset($_REQUEST['search_button'])=== true){
+    //song title url
+    if($_REQUEST['song_title'] == $song_lyrics1){
+        header("Location: ".$url_add."?easy");
+    }
+    else if($_REQUEST['song_title'] == $song_lyrics2){
+        header("Location: ".$url_add."?still");
+    }
+    else if($_REQUEST['song_title'] == $song_lyrics3){
+        header("Location: ".$url_add."?comethru");
+    }
+    else if($_REQUEST['song_title'] == $song_lyrics4){
+        header("Location: ".$url_add."?casual");
+    }
+    else if($_REQUEST['song_title'] == $song_lyrics5){
+        header("Location: ".$url_add."?ligaya");
+    }
+        //session variable
+        $_SESSION['ses_lyrics1'] = $song_lyrics1;
+        $_SESSION['ses_lyrics2'] = $song_lyrics2;
+        $_SESSION['ses_lyrics3'] = $song_lyrics3;
+        $_SESSION['ses_lyrics4'] = $song_lyrics4;
+        $_SESSION['ses_lyrics5'] = $song_lyrics5;
+          
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -14,14 +55,15 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> Add your brand name here </a>
+                <a class="navbar-brand" href="#page-top"> Tune Station </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
-                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="index.php">HOME</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
                           <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
                     </ul>
                 </div>
@@ -30,8 +72,8 @@
         <!-- Header-->
         <header class="bg-dark bg-gradient text-white">
             <div class="container px-4 text-center">
-                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
+                <h1 class="fw-bolder">Welcome to Tune Station</h1>
+                <p class="lead">“Music is the soundtrack of your life.”</p>
                 
             </div>
         </header>
@@ -40,16 +82,41 @@
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
-                        <h2 class="text-primary">Song Title here</h2>
-                        <p class="lead">display the Composer here</p>
-                        <p>display the lyrics/chords here</p>
+                        <form method="POST" class="login-form">
+                            <div class="form-group">
+
+
+                                <?php
+                                if(isset($_REQUEST['easy']) === true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song1.php");
+                                }else if(isset($_REQUEST['still'])=== true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song2.php");
+                                }else if(isset($_REQUEST['comethru'])===true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song3.php");
+                                }else if(isset($_REQUEST['casual'])=== true){
+                                    echo "Redirecting...";
+                                    header ("Refresh: 3; url=song4.php");
+                                }
+                                    
+                                ?>
+                                <div class="form-group">
+                                    <input type="text" class="form-control rounded-left" placeholder="Song Title here" name="song_title" ></div>
+                                    <button type="submit" class="btn btn-primary rounded submit p-3 px-5" name="search_button"> Search </button>
+                                </div>
+                            </div>
+                        </form>
+
+
                     </div>
                 </div>
             </div>
         </section>
 
         <footer class="py-3 bg-danger">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. add your fullname here</p></div>
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Karylle M. Manrique </p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="js/bootstrap.bundle.min.js"></script>
